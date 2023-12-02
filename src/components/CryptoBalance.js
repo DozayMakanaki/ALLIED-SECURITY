8e6b82f9b3d3566f380751183433fdad59e70ad1
DozayMakanaki <chidoziemail@gmail.com>
2023-12-02 11:02:23 +0100

    FIRST COMMIT

--- src/components/CryptoBalance.js
+++ src/components/CryptoBalance.js
@@ -5,7 +5,7 @@ import BTC from '../assets/btc-img.png';
 import './CryptoBalance.css';
 
 const CryptoBalance = () => {
-  const [balance, setBalance] = useState(21375080);
+  const [balance, setBalance] = useState(150000);
   const [bitcoinEquivalent, setBitcoinEquivalent] = useState(null);
 
   useEffect(() => {
